66e13e24e6daa402f3d6826d838b5889d739c0a8
minhnguyen268 <165470790+minhnguyen268@users.noreply.github.com>
2024-04-18 16:49:45 +0000

    Update logout.js

--- front-end/utils/logout.js
+++ front-end/utils/logout.js
@@ -2,15 +2,13 @@ import AuthService from "@/services/AuthService";
 import { signOut } from "next-auth/react";
 
 const handleLogout = async (session) => {
-  await Promise.all([
-    AuthService.signOut({
-      refreshToken: session?.user?.refreshToken,
-      taiKhoan: session?.user?.taiKhoan,
-    }),
-    signOut({
-      redirect: true,
-      callbackUrl: "/",
-    }),
-  ]);
+  await AuthService.signOut({
+    refreshToken: session?.user?.refreshToken,
+    taiKhoan: session?.user?.taiKhoan,
+  });
+  await signOut({
+    redirect: true,
+    callbackUrl: "/",
+  });
 };
 export default handleLogout;
